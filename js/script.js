9b2bb283d84c34eff2ec1e671cf5541be50e8709
DrimJimbo <cavenaileaymerik@gmail.com>
2026-01-19 21:37:37 +0100

    ajout des langages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,136 @@
+// Nav
 const btn = document.getElementById("navbutton");
 const nav = document.getElementById("nav");
 btn.addEventListener("click", () => {
     nav.classList.toggle("open");
     btn.innerHTML = nav.classList.contains("open") ? "&#10005;" : "&#9776;";
 });
+//aboutme
+const rigthabout = document.getElementById("btnright1");
+const leftabout = document.getElementById("btnleft1");
+const div = document.getElementById("aboutme");
+var presentation = document.createElement("p");
+presentation.id = "paboutme";
+presentation.innerHTML = `
+Bonjour! Je m'appelle Aymerik. J'ai 21 ans. Je suis étudiant en
+    3éme année de licence informatique à l'université d'artois à
+    Lens. Par la suite je voudrais poursuivre mes études en master
+    de cybersécurité. Je recherche un stage de 8 semaines minimun
+    dans la cybersécurité afin d'acquérir de l'expérience. Et
+    pourquoi pas continuer en contrat d'apprentissage par la suite.
+    Si vous voulais en apprendre un peu plus vous pouvez télécharger
+    mon cv et/ou ma lettre de motivation. Ou alors 
+`;
+//
+const contactLink = document.createElement("a");
+contactLink.href = "#contact";
+contactLink.textContent = "me contacter";
+//
+const cvLink = document.createElement("a");
+cvLink.href = "doc/cv_stage.pdf";
+cvLink.download = "MonCV.pdf";
+cvLink.textContent = "Mon CV";
+//
+const lmLink = document.createElement("a");
+lmLink.href = "doc/lettre_de_motivation_3eme_licence.pdf";
+lmLink.download = "LettreDeMotivation.pdf";
+lmLink.textContent = "Ma lettre de motivation";
+//
+presentation.appendChild(contactLink);
+presentation.appendChild(document.createTextNode(" pour un entretiens !"));
+presentation.appendChild(document.createElement("br"));
+presentation.appendChild(cvLink);
+presentation.appendChild(document.createElement("br"));
+presentation.appendChild(lmLink);
+div.insertBefore(presentation, rigthabout);
+//
+var langage = document.createElement("p");
+langage.id = "paboutme";
+const imgPython = document.createElement("img");
+imgPython.src = "../images/python.png";
+imgPython.className = "imgLangage";
+const imgC = document.createElement("img");
+imgC.src = "../images/c-.png";
+imgC.className = "imgLangage";
+const imgJava = document.createElement("img");
+imgJava.src = "../images/java.png";
+imgJava.className = "imgLangage";
+const imgJs = document.createElement("img");
+imgJs.src = "../images/javascript.png";
+imgJs.className = "imgLangage";
+const imgSql = document.createElement("img");
+imgSql.src = "../images/sql.png";
+imgSql.className = "imgLangage";
+const imgHtmlCss = document.createElement("img");
+imgHtmlCss.src = "../images/htmlcss.png";
+imgHtmlCss.className = "imgLangage";
+const imgPhp = document.createElement("img");
+imgPhp.src = "../images/php.png";
+imgPhp.className = "imgLangage";
+const imgShell = document.createElement("img");
+imgShell.src = "../images/shell.png";
+imgShell.className = "imgLangage";
 
+langage.appendChild(imgPython);
+langage.appendChild(document.createTextNode(" Python"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgC);
+langage.appendChild(document.createTextNode(" C"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgJava);
+langage.appendChild(document.createTextNode(" Java"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgJs);
+langage.appendChild(document.createTextNode(" JavaScript"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgSql);
+langage.appendChild(document.createTextNode(" SQL"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgHtmlCss);
+langage.appendChild(document.createTextNode(" HTML/CSS"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgPhp);
+langage.appendChild(document.createTextNode(" PHP"));
+langage.appendChild(document.createElement("br"));
+langage.appendChild(imgShell);
+langage.appendChild(document.createTextNode(" Shell Script"));
+langage.appendChild(document.createElement("br"));
+
+var j = 0;
+rigthabout.addEventListener("click", () => {
+    j++;
+    if (j >= 2) {
+        j = 0;
+    }
+    switch (j) {
+        case 0:
+            div.replaceChild(presentation, document.getElementById("paboutme"));
+            break;
+        case 1:
+            div.replaceChild(langage, document.getElementById("paboutme"));
+            break;
+        default:
+            break;
+    }
+});
+leftabout.addEventListener("click", () => {
+    j--;
+    if (j < 0) {
+        j = 1;
+    }
+    switch (j) {
+        case 0:
+            div.replaceChild(presentation, document.getElementById("paboutme"));
+            break;
+        case 1:
+            div.replaceChild(langage, document.getElementById("paboutme"));
+            break;
+        default:
+            break;
+    }
+});
+
+// Projet
 const projectimg = document.getElementById("projectimg");
 const explication = document.getElementById("explication projets");
 
